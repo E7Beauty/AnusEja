c20bc58581f7e98d77895dbfdd31ef5754659de9
E7Beauty <feniksraw@gmail.com>
2022-12-05 20:09:32 +0300

    fix bugs in polindrom

--- polindrom.js
+++ polindrom.js
@@ -1,23 +1,17 @@
-//введеное слово полиндром или нет?
-let slovo = prompt('Введите слово без пробелов маленькими буквами','шалаш');
-let slovoArr = [];
+//is the entered word a palindrome?
 
-for (let i = 0; i < slovo.length; i++) { // заполняем массив буквами из введеного слова
-    slovoArr [i] = slovo [i];
+const word = prompt('Enter your word','wow');
+let counter = 0;
+
+for ( let i = 0; i < word.length; i++ ) {
+  if (word[i] != word[word.length - i - 1]) {
+    console.log('Слово', word, 'НЕ является полиндромом!')
+    break;
+  } else {
+    counter++;
+  }
 }
 
-let arrLength = slovoArr.length; //запомниаем количество букв в слове
-let y = 0; //счетчик совпадающих букв
-let n = Math.floor(slovoArr.length / 2);//разделим кол-во букв на 2 и округлим вниз (на случай нечетного кол-ва букв)
-
-for (i = 0; i < n; i++) {
-    if (slovoArr[i] == slovoArr[arrLength - i - 1]) { //сравниваем буквы с начала и с конца
-        ++y; // если буквы совпали записываем это число
-    }
-}
-
-if (y == n) { // сравниваем количество совпадающих букв с разных сторон слова с количеством букв до середины слова (они должны быть равны в полиндромах)
-    document.write('Слово "', slovo, '" является полиндромом!', '<br \/>');
-} else {
-    document.write('Слово "', slovo, '" НЕ является полиндромом!', '<br \/>');
+if ( counter == word.length ) {
+  console.log('Слово', word, 'является полиндромом!')
 }
