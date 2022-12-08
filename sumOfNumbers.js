c4346914f0b4a641aa1cffc99a9def9fb720d8ee
E7Beauty <feniksraw@gmail.com>
2022-12-08 23:11:33 +0300

    firstCommit

---
+++ sumOfNumbers.js
@@ -0,0 +1,25 @@
+const numbers = [];
+
+for (let i = 0; i < 15; i++) {
+  numbers[i] = Math.floor(Math.random() * (i + 5));
+}
+
+console.log('Initial array is', numbers);
+
+const getSum = ((arr, number = +prompt('Enter your number','25')) => {
+  let sum = 0;
+  let index = 0;
+  const arrResult = [];
+  for (const item of arr) {    
+    if ((sum + item) < number) {
+      sum += item;
+      arrResult[index] = item;
+    } else {
+      break;
+    }
+    index++;
+  }
+  console.log('There are', arrResult.length, 'numbers whose sum is less than', number, 'these are:', arrResult, 'sum is:', sum);
+})
+
+getSum(numbers);
