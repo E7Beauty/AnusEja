69cc97387a1a8a699a7e18c5e575c0aa1b4b430c
E7Beauty <feniksraw@gmail.com>
2022-12-21 17:20:34 +0300

    fix input and span

--- toDo.js
+++ toDo.js
@@ -1,5 +1,7 @@
 document.querySelector('.element').style.display = "none";
 
+let removeCount = 0;
+
 const keys = [];
 
 const deleteNote = () => {
@@ -7,21 +9,23 @@ const deleteNote = () => {
     localStorage.removeItem(keys.findIndex(item => item?.link == event.currentTarget.parentNode.parentNode));
     keys.splice(keys.findIndex(item => item?.link == event.currentTarget.parentNode.parentNode), 1);
     event.currentTarget.parentNode.parentNode.remove();
+    ++removeCount;
   }
 }
 
 const addNewNote = () => {
   if (document.querySelector('.input__text').value != '') {
     let newRow = document.querySelector('.container__element').cloneNode(true);
-    newRow.querySelector('.note__text').value = document.querySelector('.input__text').value;
+    newRow.querySelector('.note__text').innerHTML = document.querySelector('.input__text').value;
     newRow.style.display = "";
+    newRow.querySelector('.note__input').style.display = 'none';
     document.querySelector('.container').appendChild(newRow);
 
     localStorage.setItem(0, +localStorage.getItem(0) + 1);
     localStorage.setItem(localStorage.getItem(0), document.querySelector('.input__text').value);
 
     keys[+localStorage.getItem(0)] = {};
-    keys[+localStorage.getItem(0)].key = localStorage.key(localStorage.getItem(0));
+    keys[+localStorage.getItem(0)].key = +(localStorage.key(localStorage.getItem(0) - removeCount)) + removeCount;
     keys[+localStorage.getItem(0)].link = document.querySelector('.container').lastElementChild;
 
     document.querySelector('.input__text').value = '';
@@ -31,34 +35,41 @@ const addNewNote = () => {
 }
 
 const editNote = () => {
-  const ref = event.currentTarget.parentNode.parentNode.lastElementChild.previousElementSibling.firstElementChild;
-  ref.disabled = false;
-  const inputValue = ref.value;
-  const inputRef = ref.parentNode.parentNode;
-  ref.nextElementSibling.firstElementChild.style.visibility = "visible";
-  ref.nextElementSibling.lastElementChild.style.visibility = "visible";
+  const ref = event.currentTarget.parentNode.parentNode.lastElementChild.previousElementSibling.firstElementChild.lastElementChild;
+  ref.previousElementSibling.value = ref.innerHTML;
+  ref.previousElementSibling.focus();
+  ref.style.display = 'none';
+  ref.previousElementSibling.style.display = '';
+  const inputValue = ref.innerHTML;
+  const inputRef = ref.parentNode.parentNode.parentNode;
+  ref.parentNode.nextElementSibling.firstElementChild.style.visibility = "visible";
+  ref.parentNode.nextElementSibling.lastElementChild.style.visibility = "visible";
 
-  ref.nextElementSibling.firstElementChild.addEventListener('click', () => {
+  ref.parentNode.nextElementSibling.firstElementChild.addEventListener('click', () => {
     event.currentTarget.style.visibility = "hidden";
     event.currentTarget.nextElementSibling.style.visibility = "hidden";
-    localStorage.setItem(keys[keys.findIndex(item => item?.link == inputRef)].key, event.currentTarget.parentNode.parentNode.firstElementChild.value);
+    localStorage.setItem(keys[keys.findIndex(item => item?.link == inputRef)].key, event.currentTarget.parentNode.parentNode.firstElementChild.firstElementChild.value);
     keys[keys.findIndex(item => item?.link == inputRef)].link = event.currentTarget.parentNode.parentNode.parentNode;
-    ref.disabled = true;
+    event.currentTarget.parentNode.previousElementSibling.lastElementChild.innerHTML = event.currentTarget.parentNode.previousElementSibling.firstElementChild.value;
+    ref.style.display = '';
+    ref.previousElementSibling.style.display = 'none';
   });
 
-  ref.nextElementSibling.lastElementChild.addEventListener('click', () => {
+  ref.parentNode.nextElementSibling.lastElementChild.addEventListener('click', () => {
     event.currentTarget.style.visibility = "hidden";
     event.currentTarget.previousElementSibling.style.visibility = "hidden";
-    event.currentTarget.parentNode.parentNode.firstElementChild.disabled = true;
-    event.currentTarget.parentNode.parentNode.firstElementChild.value = inputValue;
-    ref.disabled = true;
+    event.currentTarget.parentNode.parentNode.firstElementChild.lastElementChild = inputValue;
+    ref.style.display = '';
+    ref.previousElementSibling.style.display = 'none';
   });
 }
 
 const onPageCreate = (i) => {
+
   let newRow = document.querySelector('.container__element').cloneNode(true);
-  newRow.querySelector('.note__text').value = localStorage.getItem(i);
+  newRow.querySelector('.note__text').innerHTML = localStorage.getItem(i);
   newRow.style.display = "";
+  newRow.querySelector('.note__input').style.display = 'none';
   document.querySelector('.container').appendChild(newRow);
 
   localStorage.setItem(0, +localStorage.getItem(0) + 1);
@@ -67,7 +78,7 @@ const onPageCreate = (i) => {
   keys[+localStorage.getItem(0)].key = localStorage.key(localStorage.getItem(0));
   keys[+localStorage.getItem(0)].link = document.querySelector('.container').lastElementChild;
 
-  document.querySelector('.input__text').value = '';
+  document.querySelector('.input__text').inner = '';
   document.querySelector('.container').lastElementChild.querySelector('.delete').addEventListener('click', deleteNote);
   document.querySelector('.container').lastElementChild.querySelector('.edit').addEventListener('click', editNote);
 }
@@ -75,7 +86,10 @@ const onPageCreate = (i) => {
 //upon start
 localStorage.setItem(0, 0);
 
-for (let i = 1; i < localStorage.length + 1; i++) {
+const localArr = [];
+for (let item in localStorage) localArr.push(item);
+
+for (let i = 1; i < +localArr[1] + 1; i++) {
   if (localStorage.getItem(i) != null) {
     onPageCreate(i);
   }
